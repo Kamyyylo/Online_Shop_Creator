02006f72e6f9cccabc37e8e41275ea7fe50715f4
Kamyyylo <kamyszek1@gmail.com>
2019-10-03 21:29:50 +0200

    empty project

---
+++ GadgetShopBackend/src/main/java/com/gadgetshop/GadgetShopApplication.java
@@ -0,0 +1,13 @@
+package com.gadgetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadgetShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GadgetShopApplication.class, args);
+    }
+
+}
