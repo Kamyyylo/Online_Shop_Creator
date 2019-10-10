136d0fb1f8d15788533441be9c66ce9dbb4a4e55
Kamyyylo <kamyszek1@gmail.com>
2019-10-10 22:14:10 +0200

    Added category on backend with validation and empty project on frontend

---
+++ GadgetShopBackend/src/main/java/com/gadgetshop/services/CategoryService.java
@@ -0,0 +1,50 @@
+package com.gadgetshop.services;
+
+import com.gadgetshop.domain.Category;
+import com.gadgetshop.exceptions.CategoryIdException;
+import com.gadgetshop.repositories.CategoryRepository;
+import com.sun.org.apache.xml.internal.resolver.CatalogException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    //Method to save new categories and also updating them
+    public Category saveOrUpdateCategory(Category category) {
+        try {
+            category.setCategoryIdentifier(category.getCategoryIdentifier().toUpperCase());
+            //Business Logic first
+            //CRUD actions last
+            return categoryRepository.save(category);
+        } catch (Exception e) {
+            throw new CategoryIdException("Category ID '" + category.getCategoryIdentifier() + "' already exists");
+        }
+    }
+
+    //Method to finding categories by identifier
+    public Category findCategoryByIdentifier(String categoryIdentifier) {
+        Category category = categoryRepository.findByCategoryIdentifier(categoryIdentifier.toUpperCase());
+        if (category == null) {
+            throw new CategoryIdException("Category ID does not exist");
+        }
+        return category;
+    }
+
+    //Method to find all categories
+    public Iterable<Category> findAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    //delete category
+    public void deleteCategoryByIdentifier(String categoryIdentifier) {
+        Category category = categoryRepository.findByCategoryIdentifier(categoryIdentifier);
+        if (category == null) {
+            throw new CategoryIdException("Category with id '" + categoryIdentifier + "' does not exists");
+        }
+        categoryRepository.delete(category);
+    }
+}
